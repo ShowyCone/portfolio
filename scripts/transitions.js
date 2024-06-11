caea37f60d98c91c44cbd2d571aaca621bd58adc
ShowyCone <alexrafael2003@gmail.com>
2024-06-10 20:58:06 -0400

    add project 2

--- scripts/transitions.js
+++ scripts/transitions.js
@@ -1,15 +1,23 @@
 // Buttons in inventory
 const projectsID = {
   arraysJS: document.getElementById('project1'),
+  rotaractWeb: document.getElementById('project2'),
 }
 const cvButton = document.getElementById('cvButton')
 const about = document.getElementById('about')
 const techStackList = document.getElementById('techStackList')
 const elementsToShow = {
+  // Left and right sections
   project1Left: document.getElementById('project1Left'),
   project1Right: document.getElementById('project1Right'),
-  image1: document.getElementById('image1'),
-  image2: document.getElementById('image2'),
+  project2Left: document.getElementById('project2Left'),
+  project2Right: document.getElementById('project2Right'),
+
+  // Images
+  project1Image1: document.getElementById('project1-image1'),
+  project1Image2: document.getElementById('project1-image2'),
+  project2Image1: document.getElementById('project2-image1'),
+  project2Image2: document.getElementById('project2-image2'),
 }
 
 const menuState = {
@@ -18,15 +26,27 @@ const menuState = {
   someProject: false,
 }
 
-function toggleAnimation() {
+function toggleAnimation1() {
   about.classList.toggle('hiddenTop')
   techStackList.classList.toggle('hiddenRight')
   cvButton.classList.toggle('hiddenBottom')
 
   elementsToShow.project1Left.classList.toggle('showRight')
-  elementsToShow.image1.classList.toggle('showRight')
+  elementsToShow.project1Image1.classList.toggle('showRight')
   elementsToShow.project1Right.classList.toggle('showLeft')
-  elementsToShow.image2.classList.toggle('showLeft')
+  elementsToShow.project1Image2.classList.toggle('showLeft')
 }
 
-projectsID.arraysJS.addEventListener('click', toggleAnimation)
+function toggleAnimation2() {
+  about.classList.toggle('hiddenTop')
+  techStackList.classList.toggle('hiddenRight')
+  cvButton.classList.toggle('hiddenBottom')
+
+  elementsToShow.project2Left.classList.toggle('showRight')
+  elementsToShow.project2Image1.classList.toggle('showRight')
+  elementsToShow.project2Right.classList.toggle('showLeft')
+  elementsToShow.project2Image2.classList.toggle('showLeft')
+}
+
+projectsID.arraysJS.addEventListener('click', toggleAnimation1)
+projectsID.rotaractWeb.addEventListener('click', toggleAnimation2)
